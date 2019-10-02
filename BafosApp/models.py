76701c191b660768ab5c25f8485b78429ba5f837
Masanov Artem <AMasanov@initiative.ru>
2019-10-02 16:07:45 +0300

    MailTemplateGenerator was added

--- BafosApp/models.py
+++ BafosApp/models.py
@@ -7,19 +7,20 @@ from django.dispatch import receiver
 from simple_history.models import HistoricalRecords
 from wiki.models import Article, ArticleRevision
 from simple_history import register
-
+from django.db.models import signals
+from django.contrib.auth.models import User
+from datetime import datetime
 # Create your models here.
 
-register(ArticleRevision)
 
 STATUS_CHOISES = (
-    ('d','Draft'),
-    ('p','published'),
+    ('d', 'Draft'),
+    ('p', 'published'),
 )
 
 
 class Category(models.Model):
-    name = models.CharField('Name',max_length=50)
+    name = models.CharField('Name', max_length=50)
     slug = models.SlugField('Slug')
 
     def __str__(self):
@@ -39,16 +40,16 @@ class PostManager(models.Manager):
 
 
 class BlogPost(models.Model):
-    title = models.CharField('Title',max_length=50)
+    title = models.CharField('Title', max_length=50)
     slug = models.SlugField('Slug')
     tease = models.TextField('Tease(summary)', blank=True)
     body = models.TextField('Content')
-    draft = models.BooleanField('Is draft',default=True)
-    status = models.CharField(max_length=1,choices=STATUS_CHOISES,
+    draft = models.BooleanField('Is draft', default=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOISES,
                               default='d')
-    created_date = models.DateTimeField('Date of creation',default=datetime.now)
-    published_date = models.DateTimeField('Date of publication',default=datetime.now)
-    category = models.ForeignKey(Category,related_name='entries', on_delete=models.DO_NOTHING)
+    created_date = models.DateTimeField('Date of creation', default=datetime.now)
+    published_date = models.DateTimeField('Date of publication', default=datetime.now)
+    category = models.ForeignKey(Category, related_name='entries', on_delete=models.DO_NOTHING)
 
     objects = PostManager()
 
@@ -85,4 +86,29 @@ class Polls(models.Model):
     history = HistoricalRecords()
 
 
+def check_model(sender, instance, created, **kwargs):
+    user = User.objects.get(pk=1)
+    field_value_time = ArticleRevision.history.all()[0]
+    obj = ArticleRevision.objects.all()[0]
+    field_name = 'title'
+    field_value_name = getattr(obj, field_name)
+    field_name = 'content'
+    field_value_content = getattr(obj, field_name)
+    field_name = 'user'
+    field_value_user = getattr(obj, field_name)
+    MailTemplate.objects.create(
+        name="Article was changed",
+        subject="Article changed",
+        message="Article '" + str(field_value_name) + "' was changed by" + str(field_value_user) + " in " +
+                str(field_value_time) + ". Now content of the '" + str(field_value_name) + "' is " + str(field_value_content),
+        slug="Article was changed in " + str(datetime.now().date().strftime("%d/%m/%y")) + " " + str(datetime.now().time().strftime("%H:%M")),
+        is_html=False,
+    )
 
+    send_db_mail("Articlewaschangedin" + str(datetime.now().date().strftime("%d%m%y")) + "" +
+                 str(datetime.now().time().strftime("%H%M")), user.email, use_celery=False)
+
+
+signals.post_save.connect(check_model, sender=ArticleRevision)
+
+register(ArticleRevision)
